2f0b5e9f6edb6d5f0310d81d861a9f46da5bcbfa
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-26 23:44:30 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
    },
    runtimeConfig: {
     public: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      NUXT_FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
       FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     } //,
