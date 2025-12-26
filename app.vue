7efef5f2c0c5dff77f51332749bee57b68f8d7e3
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-26 21:05:55 +0100

    Update app.vue
    return to initial version

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>
-    <div style="background: red; color: white; padding: 20px; position: fixed; z-index: 9999;">
-      Debug Key: {{ config.public.FIREBASE_API_KEY || 'MISSING' }}
-    </div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<script setup>
-const config = useRuntimeConfig()
-</script>
