c3aa2ec042771bb7e639ed402481f2ef11e44ea5
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-27 00:11:44 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,11 +21,19 @@ export default defineNuxtConfig({
       transpile: ['vue-toastification']
    },
    runtimeConfig: {
+    // Private keys (server only)
+    // Public keys (available in browser)
     public: {
-      FIREBASE_API_KEY: process.env.NUXT_FIREBASE_API_KEY,
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
       FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    } //,
+    }
+   //runtimeConfig: {
+   // public: {
+   //   FIREBASE_API_KEY: process.env.NUXT_FIREBASE_API_KEY,
+   //   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+   //   FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+   // } //,
     //private: {
     //  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
      // FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
