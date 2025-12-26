889336ef5b09f91c61cf62f93c0765b0b105c241
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-26 21:24:05 +0100

    Update useFirebase.ts

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -15,7 +15,7 @@ const getFirebaseApp = () => {
   const firebaseConfig = {
     apiKey: config.public.FIREBASE_API_KEY,
     projectId: config.public.FIREBASE_PROJECT_ID,
-    authDomain: `${config.public.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    // authDomain: `${config.public.FIREBASE_PROJECT_ID}.firebaseapp.com`,
     appId: config.public.FIREBASE_APP_ID,
   }
   return getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
