0ff7d7f16a015a195a2e2c9c79e91479ee593db6
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-26 15:35:45 +0100

    Update nuxt.config.ts
    force use of an updated Node.js version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  nitro: {
+    vercel: {
+      config: {
+        runtime: 'nodejs20.x'
+      }
+    }
+  },
     app: {
         head: {
           title: 'Alexa Radio',
