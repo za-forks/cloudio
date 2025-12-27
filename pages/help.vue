b81083cffb80ccbf9e0ab88def9876cb17634357
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-27 04:58:02 +0100

    Update help.vue
    page content only after reload issue

--- pages/help.vue
+++ pages/help.vue
@@ -8,6 +8,7 @@
 
 </script> -->
 
+<!--
 <template>
   <ContentRenderer :value="data" class="w-full px-4 mx-auto prose prose-invert"/>
 </template>
@@ -22,6 +23,29 @@ const { data } = await useAsyncData('help', () => queryContent('/help').findOne(
     font-size: 1.5em;
     margin-top: 0;
     margin-bottom: 0;
+    line-height: 1.3333333;
+}
+</style> -->
+
+<template>
+  <main class="w-full px-4 mx-auto prose prose-invert">
+    <ContentDoc path="/help" v-slot="{ doc }">
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+  </main>
+</template>
+
+<script setup lang="ts">
+// No manual fetching needed here, ContentDoc handles it!
+</script>
+
+<style>
+.prose :where(h2):not(:where([class~="not-prose"] *)) {
+    color: var(--tw-prose-headings);
+    font-weight: 700;
+    font-size: 1.5em;
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
     line-height: 1.3333333;
 }
 </style>
