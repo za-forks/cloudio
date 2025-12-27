42e95d25f835c628e3f613e2f45dd21be6975839
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-27 02:12:48 +0100

    Create debug.vue

---
+++ pages/debug.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="background: white; color: black; padding: 50px; font-family: monospace;">
+    <h1>Debug Environment</h1>
+    <p>API Key Length: {{ config.public.FIREBASE_API_KEY?.length || 'UNDEFINED' }}</p>
+    <p>Project ID: {{ config.public.FIREBASE_PROJECT_ID || 'UNDEFINED' }}</p>
+    <hr />
+    <pre>{{ config.public }}</pre>
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig()
+</script>
