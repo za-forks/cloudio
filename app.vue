09aa138a8146deaef73aa157e567bca5ecd8ab60
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-27 03:51:51 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
-  <div>
-    <ClientOnly>
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
-      
-      <template #fallback>
-        <div class="h-screen flex items-center justify-center bg-gray-900 text-white">
-          <p class="animate-pulse">Loading Cloudio Radio...</p>
-        </div>
-      </template>
-    </ClientOnly>
-  </div>
 </template>
