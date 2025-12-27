793bebef28bf1dcdee9dfedb5c71477e96aa84c3
zaitmambet <49763880+zaitmambet@users.noreply.github.com>
2025-12-27 01:40:19 +0100

    Update useFirebase.ts

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -16,6 +16,7 @@ import {
 // This ensures we never run Firebase on the server (solving Hydration errors)
 // and never initialize the app twice (solving API Key errors).
 const getSafeApp = () => {
+  console.log("FIREBASE CONFIG CHECK:", config.public.FIREBASE_API_KEY);
   if (process.server) return null; 
 
   const config = useRuntimeConfig();
